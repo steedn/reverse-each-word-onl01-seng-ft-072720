cfc25b22580f9fc28bfe68daa86cacd6d9b6929b
Nathan Steed <steedn@outlook.com>
2020-07-31 16:58:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,13 +9,11 @@
 #   end.join(" ")
 # end
 
-require "pry"
-
-def reverse_each_word(string)
-  array = string.split(" ")
-  # binding.pry
+def reverse_each_word(string) #write a method that takes in a string
+  array = string.split(" ") #string into array
+  new_array = [] #used in the following .collect block
   array.collect do |word|
-  word.reverse
-    # binding.pry
-  end.join(" ")
+    new_array << word.reverse
+  end
+   return new_array.join(" ") #returning array as string
 end
