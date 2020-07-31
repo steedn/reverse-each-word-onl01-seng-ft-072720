5f0d299d2bec5cf42e6a2e89bd943f3eff0e7780
Nathan Steed <steedn@outlook.com>
2020-07-31 16:58:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,18 @@
+# require "pry"
+# 
+# def reverse_each_word(string)
+#   array = string.split(" ")
+#   # binding.pry
+#   array.collect do |word|
+#   word.reverse
+#     # binding.pry
+#   end.join(" ")
+# end
+
 require "pry"
 
 def reverse_each_word(string)
   array = string.split(" ")
-  new_array = []
   # binding.pry
   array.collect do |word|
   word.reverse
